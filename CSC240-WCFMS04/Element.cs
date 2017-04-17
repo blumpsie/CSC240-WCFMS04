87f8f4058bf6afb4c0d62bab7eaafd4861baee95
DEATHMACHINE\Blumpsie <blumpsie@gmail.com>
2017-04-17 10:31:33 -0400

    Initial Commit
    Almost the same code as project two just some minor tweaks including the
    replacement of ElementSet with Set. And some changes because of C#
    syntax.

---
+++ CSC240-WCFMS04/Element.cs
@@ -0,0 +1,28 @@
+﻿using System;
+/*
+ * Exactly the same as Element.java, hopefully they work exaclty the same too.
+ */
+namespace CSC240_WCFMS04
+{
+    abstract class Element
+    {
+        public string getClassName()
+        {
+            string resultStr;
+            int beginAt;
+
+            resultStr = this.ToString();
+            beginAt = resultStr.IndexOf('.');
+
+            return resultStr.Substring(beginAt + 1);
+        }
+
+        public abstract void readIn();
+
+        public abstract void display();
+
+        public abstract Element clone();
+
+        public abstract string toString();
+    }
+}
---
+++ CSC240-WCFMS04/Set.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSC240_WCFMS04
+{
+    class Set <T>
+    {
+        // Fields
+        List<T> theList;
+        int currentIndex; // Index of current element in the set
+        const int START_CAP = 100; // Initial capacity
+
+        // Constructor
+        public Set()
+        {
+            theList = new List<T>(START_CAP);
+            currentIndex = -1;
+        }
+
+        // Test to see if the list is empty or not
+        public bool isEmpty()
+        {
+
+            if(theList == null || theList.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // Test to see if parameter is a member of the set
+        public bool isMemberOf(T aT)
+        {
+            string paramClass = aT.GetType().ToString(); // Hopefully this works
+            String currClass;
+
+            foreach (T currT in theList)
+            {
+                currClass = currT.GetType().ToString();
+
+                if (currClass.Equals(paramClass))
+                {
+                    if (currT.Equals(aT))
+                    {
+                        return true; // they're the same
+                    }
+                }
+            }
+            return false; // no matches
+        }
+
+        public int size()
+        {
+            return theList.Count;
+        }
+
+        public T getCurrent()
+        {
+            int saveIndex = currentIndex;
+
+            if (currentIndex == theList.Count - 1)
+            {
+                currentIndex = 0;
+            }
+            else
+            {
+                currentIndex++;
+            }
+
+            return theList[saveIndex];
+        }
+
+        // Adds paramter to set
+        public bool add(T aT)
+        {
+            if(this.isMemberOf(aT))
+            {
+                return false; // Duplicate object
+            }
+
+            theList.Add(aT);
+
+            if (theList.Count == 1) currentIndex = 0;
+
+            // Successful
+            return true;
+        }
+
+        public void clear()
+        {
+            currentIndex = -1;
+            theList = new List<T>();
+        }
+
+        public void display()
+        {
+            if(theList.Count == 0)
+            {
+                Console.WriteLine("There are no objects in the set.");
+            }
+            else
+            {
+                Console.WriteLine("Here are the object in the set: \n");
+                for (int i = 0; i < theList.Count; i++)
+                {
+                    Console.WriteLine(theList[i].ToString());
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+    }
+}
